ee767153d573416e1a76d25f2617c7104fe92871
P4rdal <marcelodeiro@hotmail.com>
2017-12-26 17:51:01 -0300

    Opções ao pressionnar o botão

--- app/src/main/java/com/example/marce/teste/Lista_todos.java
+++ app/src/main/java/com/example/marce/teste/Lista_todos.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,27 +38,61 @@ import static java.lang.Boolean.FALSE;
 public class Lista_todos extends AppCompatActivity {
 
 
-    /*private ListView lv;*/
 
     private ListView listaMovies;
-
     MovieDao movieDao;
     private static final String TAG = MainActivity.class.getSimpleName();
     private final static String API_KEY = "f96f35dad2d93764c36ed623ec9148ff";
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu,v,menuInfo);
+        menu.setHeaderTitle("Selecione uma opção");
+        menu.add(0,v.getId(),0,"Favoritos");
+        menu.add(0,v.getId(),0,"Detalhes");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        
+
+                if(item.getTitle()=="Favoritos"){
+
+
+
+                }
+                else if (item.getTitle()=="Detalhes"){
+
+
+
+                }
+                else
+                {
+
+                   return false;
+
+                }
+
+                return true;
+
+    }
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_todos);
-
-
-        //*lv = (ListView) findViewById(R.id.lista_todos);
-
         listaMovies = (ListView) findViewById(R.id.lista_todos);
         movieDao = new MovieDao(this);
         movieDao.open();
 
+        List<Movie> filmes = movieDao.getAll();
+        AdapterMovie adpter = new AdapterMovie(this,filmes);
+        listaMovies.setAdapter(adpter);
+
+        registerForContextMenu(listaMovies);
 
         if (API_KEY.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Coloque sua chave", Toast.LENGTH_LONG).show();
@@ -61,6 +100,8 @@ public class Lista_todos extends AppCompatActivity {
 
 
         }
+
+
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
@@ -83,33 +124,29 @@ public class Lista_todos extends AppCompatActivity {
 
             }
 
+
+
             @Override
             public void onFailure(Call<MoviesResponse> call, Throwable t) {
-                // Log error here since request failed
                 Log.e(TAG, t.toString());
             }
+
+
+
+
+
+
         });
 
-        /*List<String> allmovie = new ArrayList<String>();
-        List<Movie> todosfilmes = movieDao.getAll();;
-        for (Movie film : todosfilmes){
-
-            allmovie.add(film.getTitle());
-
-        }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,allmovie);
-        lv.setAdapter(arrayAdapter);*/
 
 
-        List<Movie> filmes = movieDao.getAll();
-        AdapterMovie adpter = new AdapterMovie(this,filmes);
-        listaMovies.setAdapter(adpter);
 
 
 
 
     }
+
+
 }
 
 
